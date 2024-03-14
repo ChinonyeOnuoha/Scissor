f59f4dd6da363c0e742629fa1abf9d396a20060d
Chinonye Onuoha <onuohamaria@gmail.com>
2024-03-14 10:06:48 +0100

    styling update

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,10 +1,11 @@
 //PageNotFound.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './pageNotFound.css';
 
 const PageNotFound: React.FC = () => {
     return (
-        <div>
+        <div className='page-not-found container'>
             <h1>404 - Page Not Found</h1>
             <p>The page you are looking for does not exist</p>
             <Link to="/">Go to the home page</Link>
